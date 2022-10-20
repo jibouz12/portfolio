49ebf87f30e4e92d046d9aabdbbb9db69ddac6e1
jb <jb.tournez@gmail.com>
2022-10-20 19:30:34 +0200

    portfolio

--- front/src/app/feed/components/game/game.component.ts
+++ front/src/app/feed/components/game/game.component.ts
@@ -17,65 +17,92 @@ export class GameComponent implements OnInit {
 ///////////////////
 /// animation onClick
 play(choix : number) {
-  const audio = document.getElementById("audio-1") as HTMLVideoElement;
-  audio.play();
-  const buttons = document.querySelectorAll('button');
-  const robot = buttons[Math.floor(Math.random() * 3)].innerHTML;
-  if (choix === 1 && robot === 'feuille') {
-      document.querySelector(".principal")!.className = "principal";
-      requestAnimationFrame((time) => {
-      document.querySelector(".principal")!.className = "principal animation1";
-      });
-  } 
-  if (choix === 1 && robot === 'pierre') {
-      document.querySelector(".principal")!.className = "principal";
-      requestAnimationFrame((time) => {
-      document.querySelector(".principal")!.className = "principal animation2";
-      });
-  }
-  if (choix === 1 && robot === 'ciseaux') {
-      document.querySelector(".principal")!.className = "principal";
-      requestAnimationFrame((time) => {
-      document.querySelector(".principal")!.className = "principal animation3";
-      });
-  }
-  if (choix === 2 && robot === 'feuille') {
-      document.querySelector(".principal")!.className = "principal";
-      requestAnimationFrame((time) => {
-      document.querySelector(".principal")!.className = "principal animation4";
-      });
-  }
-  if (choix === 2 && robot === 'pierre') {
-      document.querySelector(".principal")!.className = "principal";
-      requestAnimationFrame((time) => {
-      document.querySelector(".principal")!.className = "principal animation5";
-      });
-  }
-  if (choix === 2 && robot === 'ciseaux') {
-      document.querySelector(".principal")!.className = "principal";
-      requestAnimationFrame((time) => {
-      document.querySelector(".principal")!.className = "principal animation6";
-      });
-  }
-  if (choix === 3 && robot === 'feuille') {
-      document.querySelector(".principal")!.className = "principal";
-      requestAnimationFrame((time) => {
-      document.querySelector(".principal")!.className = "principal animation7";
-      });
-  }
-  if (choix === 3 && robot === 'pierre') {
-      document.querySelector(".principal")!.className = "principal";
-      requestAnimationFrame((time) => {
-      document.querySelector(".principal")!.className = "principal animation8";
-      });
-  }
-  if (choix === 3 && robot === 'ciseaux') {
-      document.querySelector(".principal")!.className = "principal";
-      requestAnimationFrame((time) => {
-      document.querySelector(".principal")!.className = "principal animation9";
-      });
-  }
-
+    document.getElementById('rslt')!.innerText = '';
+    const audio = document.getElementById("audio-1") as HTMLVideoElement;
+    audio.play();
+    const buttons = document.querySelectorAll('button');
+    const robot = buttons[Math.floor(Math.random() * 3)].innerHTML;
+    if (choix === 1 && robot === 'feuille') {
+            document.querySelector(".principal")!.className = "principal";
+            requestAnimationFrame((time) => {
+                document.querySelector(".principal")!.className = "principal animation1";
+            });
+            setTimeout(() => {
+                document.getElementById('rslt')!.innerText = 'perdu';
+            }, 6000)
+    } 
+    if (choix === 1 && robot === 'pierre') {
+            document.querySelector(".principal")!.className = "principal";
+            requestAnimationFrame((time) => {
+                document.querySelector(".principal")!.className = "principal animation2";
+            });
+            setTimeout(() => {
+                document.getElementById('rslt')!.innerText = 'égalité';
+            }, 6000)
+    }
+    if (choix === 1 && robot === 'ciseaux') {
+            document.querySelector(".principal")!.className = "principal";
+            requestAnimationFrame((time) => {
+                document.querySelector(".principal")!.className = "principal animation3";
+            });
+            setTimeout(() => {
+                document.getElementById('rslt')!.innerText = 'gagné';
+            }, 6000)
+    }
+    if (choix === 2 && robot === 'feuille') {
+            document.querySelector(".principal")!.className = "principal";
+            requestAnimationFrame((time) => {
+                document.querySelector(".principal")!.className = "principal animation4";
+            });
+            setTimeout(() => {
+                document.getElementById('rslt')!.innerText = 'égalité';
+            }, 6000)
+    }
+    if (choix === 2 && robot === 'pierre') {
+            document.querySelector(".principal")!.className = "principal";
+            requestAnimationFrame((time) => {
+                document.querySelector(".principal")!.className = "principal animation5";
+            });
+            setTimeout(() => {
+                document.getElementById('rslt')!.innerText = 'gagné';
+            }, 6000)
+    }
+    if (choix === 2 && robot === 'ciseaux') {
+            document.querySelector(".principal")!.className = "principal";
+            requestAnimationFrame((time) => {
+                document.querySelector(".principal")!.className = "principal animation6";
+            });
+            setTimeout(() => {
+                document.getElementById('rslt')!.innerText = 'perdu';
+            }, 6000)
+    }
+    if (choix === 3 && robot === 'feuille') {
+            document.querySelector(".principal")!.className = "principal";
+            requestAnimationFrame((time) => {
+                document.querySelector(".principal")!.className = "principal animation7";
+            });
+            setTimeout(() => {
+                document.getElementById('rslt')!.innerText = 'gagné';
+            }, 6000)
+    }
+    if (choix === 3 && robot === 'pierre') {
+            document.querySelector(".principal")!.className = "principal";
+            requestAnimationFrame((time) => {
+                document.querySelector(".principal")!.className = "principal animation8";
+            });
+            setTimeout(() => {
+                document.getElementById('rslt')!.innerText = 'perdu';
+            }, 6000)
+    }
+    if (choix === 3 && robot === 'ciseaux') {
+            document.querySelector(".principal")!.className = "principal";
+            requestAnimationFrame((time) => {
+                document.querySelector(".principal")!.className = "principal animation9";
+            });
+            setTimeout(() => {
+                document.getElementById('rslt')!.innerText = 'égalité';
+            }, 6000)
+    }
 }
 
 }
